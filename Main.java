07e02bcf6789642e6ed628837a2a16ecea4275a8
Xenia Bocharova <bochxenia@yandex.ru>
2020-08-20 17:54:14 +0300

    Добавлен код

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonusMile = 20;
+        int ticketCoast = 9578;
+        int bonus = ticketCoast / bonusMile;
+        System.out.println(bonus);
+    }
+}
