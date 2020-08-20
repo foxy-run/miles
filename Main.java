f145d5e125d797fcbbade92528357e94d630ec39
foxy-run <69159399+foxy-run@users.noreply.github.com>
2020-08-20 18:20:21 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int bonusMile = 20;
-        int ticketCoast = 9578;
-        int bonus = ticketCoast / bonusMile;
+        int ticketCost = 9578;
+        int bonus = ticketCost / bonusMile;
         System.out.println(bonus);
     }
 }
